b8d865f04d8e4a9be3ce96ef9c95b21d69c746d9
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-06-27 17:20:59 +0300

    Lesson 1: Level6 Task 1 - Sum and Divide done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_1/homework/Divide.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_1.homework;
+
+public class Divide {
+    public static void main(String[] args) {
+        System.out.println(50 / 3);
+    }
+}
