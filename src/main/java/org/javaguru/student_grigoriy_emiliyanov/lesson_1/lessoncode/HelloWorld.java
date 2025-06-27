ee0dc780ff064e20ad0dfac0c85fa0595cec120c
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-06-27 16:21:22 +0300

    Lesson 1: HelloWorld done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_1/lessoncode/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_1.lessoncode;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
