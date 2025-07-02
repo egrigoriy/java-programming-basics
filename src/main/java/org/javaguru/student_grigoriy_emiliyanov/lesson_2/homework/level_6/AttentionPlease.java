81de8002a61bd9cca561b6770f2dfe3dd811489e
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 14:01:59 +0300

    Lesson 2: homework: Level 6 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/homework/level_6/AttentionPlease.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.homework.level_6;
+
+class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
