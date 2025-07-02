9cedc6f81a2b515b19d2efb5f10cee17c62bffae
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 13:52:06 +0300

    Lesson 2: homework: Level 4 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/homework/level_4/PrintWithoutCR.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.homework.level_4;
+
+//Напишите программу, которая демонстрирует использование команды
+//        System.out.print("text");
+public class PrintWithoutCR {
+    public static void main(String[] args) {
+        System.out.print("text");
+    }
+}
