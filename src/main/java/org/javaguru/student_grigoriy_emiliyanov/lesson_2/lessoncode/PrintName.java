4669191f74d6eefa9191981adb44579e43e00ca1
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 11:22:36 +0300

    Lesson 2: lesson code: Task 1 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/lessoncode/PrintName.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.lessoncode;
+
+// Написать программу, которая печатает ваше имя на консоль.
+public class PrintName {
+    public static void main(String[] args) {
+        String firstName = "Vasya";
+        String lastName = "Pupkin";
+        System.out.println(firstName + " " + lastName);
+    }
+}
