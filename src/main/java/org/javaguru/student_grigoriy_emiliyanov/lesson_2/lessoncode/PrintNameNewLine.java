e8969c4610debb91e0f1f921027d6c3b4c3bbea9
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 11:25:28 +0300

    Lesson 2: lesson code: Task 2 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/lessoncode/PrintNameNewLine.java
@@ -0,0 +1,12 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.lessoncode;
+
+//Написать программу, которая печатает
+//        ваше имя в одной строке и фамилию в другой строке на консоль.
+public class PrintNameNewLine {
+    public static void main(String[] args) {
+        String firstName = "Vasya";
+        String lastName = "Pupkin";
+        System.out.println(firstName);
+        System.out.println(lastName);
+    }
+}
