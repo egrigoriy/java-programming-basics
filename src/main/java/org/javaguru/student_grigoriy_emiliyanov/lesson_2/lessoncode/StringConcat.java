48a02f6be358eb37ebf2fa62a39af727b799a82a
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 11:50:10 +0300

    Lesson 2: lesson code: Task 8 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/lessoncode/StringConcat.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.lessoncode;
+
+//Напишите программу, которая конкатенирует (склеивает) строки.
+public class StringConcat {
+    public static void main(String[] args) {
+        String firstName = "Vasya";
+        String lastName = "Pupkin";
+        String result = firstName + " " + lastName;
+        System.out.println(result);
+    }
+}
