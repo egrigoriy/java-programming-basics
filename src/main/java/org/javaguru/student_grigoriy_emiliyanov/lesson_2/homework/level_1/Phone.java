34583d035353a358c4c90babeddf3d8e3ee2267e
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 13:29:42 +0300

    Lesson 2: homework: Level 1 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/homework/level_1/Phone.java
@@ -0,0 +1,4 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.homework.level_1;
+
+public class Phone {
+}
---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/homework/level_1/Turtle.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.homework.level_1;
+
+public class Turtle {
+    public static void main(String[] args) {
+
+    }
+}
