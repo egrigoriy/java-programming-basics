b9ae137e12b35843339ef0403d5f81fbb4794ad8
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-02 11:33:04 +0300

    Lesson 2: lesson code: Task 5 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_2/lessoncode/Sum.java
@@ -0,0 +1,12 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_2.lessoncode;
+
+//Напишите программу, которая складывает два числа и выводит
+//        результат на консоль.
+public class Sum {
+    public static void main(String[] args) {
+        int firstNum = 100;
+        int secondNum = 200;
+        int result = firstNum + secondNum;
+        System.out.println(result);
+    }
+}
