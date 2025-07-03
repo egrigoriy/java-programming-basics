6d07d3bf654a04773ad55d57b2c0a27b9f16fdfb
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-03 17:05:50 +0300

    Lesson 3: Lesson code - Task 7 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/lessoncode/Apple.java
@@ -0,0 +1,13 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.lessoncode;
+
+public class Apple {
+    private String type;
+    private String color;
+    private int weight;
+
+    public Apple(String type, String color, int weight) {
+        this.type = type;
+        this.color = color;
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/lessoncode/AppleApplication.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.lessoncode;
+
+public class AppleApplication {
+    public static void main(String[] args) {
+        Apple apple1 = new Apple("antonovka", "red", 123);
+    }
+}
