f15dbbe0694e36b6570b739f26844f5bf3ac1727
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-04 17:42:41 +0300

    Lesson 4: lesson code : Tasks 1 -3 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_4/lessoncode/BooleanDemo.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_4.lessoncode;
+
+public class BooleanDemo {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println("isTrue: " + isTrue);
+        System.out.println("isFalse: " + isFalse);
+    }
+}
