52168ab40d44eebb609e180900a145465b8af32a
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-04 10:57:41 +0300

    Lesson 3: Lesson code - Task 12 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/lessoncode/Question.java
@@ -0,0 +1,19 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.lessoncode;
+
+public class Question {
+    private String text;
+    private boolean answer;
+
+    public Question(String text, boolean answer) {
+        this.text = text;
+        this.answer = answer;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean getAnswer() {
+        return answer;
+    }
+}
