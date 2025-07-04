de2f825a8d379e3aba463fe43cebbb988ff7dd7c
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-04 11:54:17 +0300

    Lesson 3: homework: Level 2 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/homework/level_1/RobotApp.java
@@ -0,0 +1,8 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.homework.level_1;
+
+public class RobotApp {
+    public static void main(String[] args) {
+        Robot vanya = new Robot("Vania");
+        Robot vasya = new Robot("Vasya");
+    }
+}
