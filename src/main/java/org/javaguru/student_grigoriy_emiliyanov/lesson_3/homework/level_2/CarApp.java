51881d3bf4a2e31f2fe7aa420eb8f11118335157
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-04 12:07:35 +0300

    Lesson 3: homework: Level 2 Task 2 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/homework/level_2/CarApp.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.homework.level_2;
+
+public class CarApp {
+    public static void main(String[] args) {
+        Car car1 = new Car("BMW", "black", 120000);
+        Car car2 = new Car("Toyota", "white", 321341);
+        Car car3 = new Car("Lada", "cherry", 1234);
+    }
+}
