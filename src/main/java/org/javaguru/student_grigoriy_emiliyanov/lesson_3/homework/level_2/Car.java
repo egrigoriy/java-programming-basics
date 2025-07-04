b74240ea0de6bb3c65c6dbb93999152490416bc5
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-04 12:04:51 +0300

    Lesson 3: homework: Level 2 Task 1 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/homework/level_2/Car.java
@@ -0,0 +1,13 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.homework.level_2;
+
+public class Car {
+    private String maker;
+    private String color;
+    private int price;
+
+    public Car(String maker, String color, int price) {
+        this.maker = maker;
+        this.color = color;
+        this.price = price;
+    }
+}
