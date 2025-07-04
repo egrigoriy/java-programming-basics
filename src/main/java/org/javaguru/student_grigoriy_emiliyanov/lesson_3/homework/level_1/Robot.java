fc003c8da059a83f2992f627c375c4af54b70ee5
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-04 11:51:55 +0300

    Lesson 3: homework: Level 1 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_3/homework/level_1/Robot.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_3.homework.level_1;
+
+public class Robot {
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+}
