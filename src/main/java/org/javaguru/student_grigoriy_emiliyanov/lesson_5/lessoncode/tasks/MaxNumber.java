703abd7e687ebf881daf616d0e158464957ce4b3
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-07 17:42:04 +0300

    Lesson 5: class code - Task 4 done

---
+++ src/main/java/org/javaguru/student_grigoriy_emiliyanov/lesson_5/lessoncode/tasks/MaxNumber.java
@@ -0,0 +1,8 @@
+package org.javaguru.student_grigoriy_emiliyanov.lesson_5.lessoncode.tasks;
+
+public class MaxNumber {
+
+    int findMax(int number1, int number2) {
+        return number1 > number2 ? number1 : number2;
+    }
+}
